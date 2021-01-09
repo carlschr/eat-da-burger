c88920d4485c0494f32955a5c465b2b7a7a3b3cd
carlschr <cjcarlson136@gmail.com>
2021-01-09 17:35:09 -0600

    Fixed values to insert from data

--- config/orm.js
+++ config/orm.js
@@ -8,13 +8,14 @@ module.exports = {
         });
     },
     insertOne(burgerData, cb) {
-        connection.query('INSERT INTO burger (burger_name, devoured) VALUES (?, ?)', [burgerData.name, burgerData.devoured], (err, data) => {
+        console.log(burgerData)
+        connection.query('INSERT INTO burgers (burger_name, devoured) VALUES (?, ?)', [burgerData.name, 0], (err, data) => {
             if (err) console.error(err);
             cb(data);
         });
     },
     updateOne(burgerID, cb) {
-        connection.query('UPDATE burgers SET devoured = ? WHERE id = ?', [false, burgerID], (err, data) => {
+        connection.query('UPDATE burgers SET devoured = ? WHERE id = ?', [1, burgerID], (err, data) => {
             if (err) console.error(err);
             cb(data);
         });
